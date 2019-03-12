1ee9a7d687c486d7909670acfa36d8c89fa0b300
Henrique de Assunção Clementino <henri.acle@gmail.com>
2019-03-11 22:56:03 -0300

    feat(*): Adicionado a pasta corretamente no git

--- Scripts/AI/AIState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class AIState : MonoBehaviour
-{
-    //Abstract Methods
-    public abstract AIStateType GetStateType();
-}
