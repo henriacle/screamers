0ade720be2b6e522206992cc660ab4e5fc046fc9
Henrique de Assunção Clementino <henri.acle@gmail.com>
2019-05-08 20:56:58 -0300

    feat(blood-effect): adicionar efeito de blood effect

--- Assets/InHuman/Scripts/FPSController/CharacterManager.cs
+++ Assets/InHuman/Scripts/FPSController/CharacterManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class CharacterManager : MonoBehaviour
 {
     [SerializeField] [Range(0.0f, 100.0f)]  private float       _playerHealth   = 100.0f;
+    [SerializeField] private CameraBloodEffect                  _cameraBloodEffect = null;
     [SerializeField]                        private GameObject  _healthBar      = null;
     [SerializeField]                                float       _currentHealth  = 0.0f;
                                                     RectTransform _lifeRect     = null;
@@ -45,13 +46,21 @@ public class CharacterManager : MonoBehaviour
 
     private void doDamage(float damage)
     {
+
         playerCurrentHealth = _currentHealth - damage;
-        _lifeRectWidth = 250 - (250 - (250 * playerCurrentHealth / 100));
-        _lifeRect.sizeDelta = new Vector2(_lifeRectWidth, _lifeRect.rect.height);
-        _lifeRect.ForceUpdateRectTransforms();
+        //_lifeRectWidth = 250 - (250 - (250 * playerCurrentHealth / 100));
+        //_lifeRect.sizeDelta = new Vector2(_lifeRectWidth, _lifeRect.rect.height);
+        //_lifeRect.ForceUpdateRectTransforms();
+
+        if (_cameraBloodEffect != null)
+        {
+            _cameraBloodEffect.minBloodAmount = (1.0f - playerCurrentHealth / 100.0f);
+            _cameraBloodEffect.bloodAmount = Mathf.Min(_cameraBloodEffect.minBloodAmount + 0.3f, 1.0f);
+        }
+
         if (playerCurrentHealth <= 0)
         {
-            //SceneManager.LoadScene(0);
+            SceneManager.LoadScene(0);
         }
     }
 }
