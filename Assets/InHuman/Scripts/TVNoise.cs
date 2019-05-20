55ba4db4dd16c35e098b6e6960f2cb670c4510bf
Henrique de Assunção Clementino <henri.acle@gmail.com>
2019-05-20 20:40:45 -0300

    adicionado televisão com noise

---
+++ Assets/InHuman/Scripts/TVNoise.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVNoise : MonoBehaviour
+{
+    [SerializeField] private Material _noise;
+    Renderer rend;
+    float _x = 0;
+    float _y = 0;
+    float scrollSpeed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.EnableKeyword("_EMISSION");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+    }
+}
